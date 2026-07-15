911a403d73d833fa096121d1a49ce9e5a8cdb6fc
Benjamin Chong <ben.c.chong@gmail.com>
2026-07-15 15:12:14 -0700

    experimental frame advance using harmony patching

--- src/FreezeFrame/FreezeFrame.cs
+++ src/FreezeFrame/FreezeFrame.cs
@@ -1,69 +1,96 @@
 ﻿using System;
-using System.Threading;
 using System.IO;
 using System.IO.Pipes;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+
 using BepInEx;
+using HarmonyLib;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.LowLevel;
-using UnityEngine.PlayerLoop;
 
 namespace FreezeFrame {
+    [DefaultExecutionOrder(-10000)]
     [BepInPlugin("com.deweydot.freezeframe", "FreezeFrame", "0.0.1")]
-    public class Plugin : BaseUnityPlugin {
+    public class FreezeFrame : BaseUnityPlugin {
         private NamedPipeServerStream pipeStream;
         private StreamReader pipeReader;
         private StreamWriter pipeWriter;
-        private bool isActive = false;
+        private static int stepFrames = -1;
+        public static float logicalTime = 0f;
+        public static float logicalDeltaTime = 0.008f;
 
         private void Awake() {
-            ConnectToPipe();
-            var pipeThread = new Thread(PipeProc) { IsBackground = true };
-            pipeThread.Start();
+            var harmony = new Harmony("com.deweydot.freezeframe");
+            PatchAssembly(harmony);
+            harmony.PatchAll();
+            //ConnectToPipe();
             Logger.LogInfo("FreezeFrame plugin started.");
         }
         
-        private void Update () {
-            if (Keyboard.current != null && Keyboard.current.f1Key.wasPressedThisFrame) {
-                FreezeGame();
+        private void Update() {
+            if (stepFrames > 0){
+                stepFrames--;
+            }
+            if (Keyboard.current != null) {
+                // f1 to pause, f2 to step, f3 to resume
+                if (Keyboard.current.f1Key.wasPressedThisFrame && !isActive()) {
+                    stepFrames = 0;
+                    Time.captureDeltaTime = 0.008f;
+                    Physics.simulationMode = SimulationMode.Script;
+                }
+                else if (Keyboard.current.f2Key.wasPressedThisFrame && isActive()) {
+                    stepFrames = 1;
+                }
+                else if (Keyboard.current.f3Key.wasPressedThisFrame && isActive()) {
+                    stepFrames = -1;
+                    Time.captureDeltaTime = 0f;
+                    Physics.simulationMode = SimulationMode.FixedUpdate;
+                }
             }
         }
 
-        private void ConnectToPipe(){
+        private void LateUpdate() {
+            if (stepFrames > 0){
+                Physics.Simulate(0.008f);
+                logicalTime += logicalDeltaTime;
+            }
+        }
+
+        private void PatchAssembly(Harmony harmony) {
+            var gameAssembly = AppDomain.CurrentDomain.GetAssemblies()
+                .First(a => a.GetName().Name == "Assembly-CSharp");
+            foreach (var type in gameAssembly.GetTypes()) {
+                if (!typeof(MonoBehaviour).IsAssignableFrom(type)) continue;
+                foreach (var methodName in new[] { "Update", "FixedUpdate", "LateUpdate" }) {
+                    var method = type.GetMethod(methodName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+                    if (method == null) continue;
+                    harmony.Patch(method, prefix: new HarmonyMethod(typeof(FreezeFrame), nameof(FreezePatch)));
+                }
+            }
+        }
+
+        static bool FreezePatch() => stepFrames != 0;
+
+        public static bool isActive() => stepFrames >= 0;
+
+        private void ConnectToPipe() {
             pipeStream = new NamedPipeServerStream("FreezeFrameTAS", PipeDirection.InOut);
             pipeStream.WaitForConnection();
             pipeReader = new StreamReader(pipeStream);
             pipeWriter = new StreamWriter(pipeStream);
+            var pipeThread = new Thread(PipeProc) { IsBackground = true };
+            pipeThread.Start();
         }
 
         private void PipeProc() {
             while (true) {
                 string line = pipeReader.ReadLine();
-                if (line != null && line == "play") {
-                    UnfreezeGame();
+                if (line != null) {
+                    continue;
                 }
             }
-        }
-
-        private void FreezeGame() {
-            if (isActive) return;
-            PlayerLoopSystem currLoop = PlayerLoop.GetCurrentPlayerLoop();
-            for (int i = 0; i < currLoop.subSystemList.Length; i++) {
-                if (currLoop.subSystemList[i].type == typeof(Update)) {
-                    currLoop.subSystemList[i].subSystemList = null;
-                }
-                else if (currLoop.subSystemList[i].type == typeof(FixedUpdate)) {
-                    currLoop.subSystemList[i].subSystemList = null;
-                }
-            }
-            PlayerLoop.SetPlayerLoop(currLoop);
-            isActive = true;
-        }
-        
-        private void UnfreezeGame() {
-            if (!isActive) return;
-            PlayerLoop.SetPlayerLoop(PlayerLoop.GetDefaultPlayerLoop());
-            isActive = false;
         }
     }
 }
