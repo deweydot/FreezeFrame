1e10983a1f9d7c8ede011e7d322a9ffeb805f476
Benjamin Chong <ben.c.chong@gmail.com>
2026-07-19 22:31:48 -0700

    Working pipe communication w/ reconnect logic

--- src/FreezeFrame/FreezeFrame.cs
+++ src/FreezeFrame/FreezeFrame.cs
@@ -5,28 +5,16 @@ namespace FreezeFrame {
     [DefaultExecutionOrder(-10000)]
     [BepInPlugin("com.deweydot.freezeframe", "FreezeFrame", "0.0.1")]
     public class Plugin : BaseUnityPlugin {
-        private PipeController pipe;
-        private int fc;
+        private ManagedPipeServer pipe;
 
         private void Awake() {
-            pipe = new PipeController();
-            fc = 0;
+            pipe = new ManagedPipeServer();
+            pipe.Start();
         }
         
         private void Update() {
             string msg = pipe.Read();
-            if (msg != null) HandleMessage(msg);
-            if (fc == 0) pipe.Write("ping!");
-            fc = (fc + 1) % 500;
-        }
-
-        private void HandleMessage(string msg) {
-            string[] parts = msg.Split(' ');
-            if (parts.Length >= 2 && parts[0] == "ts") {
-                if (float.TryParse(parts[1], out float val)) {
-                    Time.timeScale = val;
-                }
-            }
+            if (msg != null) pipe.Write(msg);
         }
     }
 }
